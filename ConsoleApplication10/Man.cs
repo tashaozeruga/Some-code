a6e61baa3ee79fea8fb63eb983cc2510055a5437
tashaozeruga <tashaozeruga@gmail.com>
2015-03-09 22:21:02 +0200

    Some code with Man class

---
+++ ConsoleApplication10/Man.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Man
+    {
+        public string name;
+        public int age;
+
+        public void say_name()
+        { 
+        Console.WriteLine ("My name is " + name);
+        
+        }
+    }
+}
---
+++ ConsoleApplication10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man m1 = new Man();
+            Man m2 = new Man();
+
+            m1.name = "Vasya";
+            m2.name = "Kolya";
+
+            m1.say_name();
+            m2.say_name();
+
+            string name = "Masha";
+            Console.WriteLine("My name is " + name);
+
+        }
+    }
+}
